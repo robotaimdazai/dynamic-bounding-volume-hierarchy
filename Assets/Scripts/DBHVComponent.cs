95dfaa728cee2e946d2eb59c913906feb35cac8a
Bilal Azhar <90259416+robotaimdazai@users.noreply.github.com>
2023-04-25 15:49:57 +0500

    components update on transform changed

--- Assets/Scripts/DBHVComponent.cs
+++ Assets/Scripts/DBHVComponent.cs
@@ -9,23 +9,46 @@ public class DBHVComponent : DBVHBase
    private AABB _aabb = new();
    private Vector3[] _corners = new Vector3[4];
    private int _index = -1;
-   public AABB AABB => _aabb;
+   private Transform _cachedTransform;
+   private Vector3 _cachedPos;
+   private Vector3 _cachedRot;
+   private Vector3 _cachedScale;
 
    private void Start()
    {
       _canvas = GetComponent<Canvas>();
       _rectTransform = GetComponent<RectTransform>();
       _index = gameObject.GetInstanceID();
+      _cachedTransform = transform;
+      _cachedPos = _cachedTransform.position;
+      _cachedRot = _cachedTransform.rotation.eulerAngles;
+      _cachedScale = _cachedTransform.localScale;
       SetAABB(); 
       Tree.InsertLeaf(_index,_aabb);
    }
 
-   [ContextMenu("UpdateTree")]
-   public void UpdateTree()
+   private void Update()
    {
+      UpdateTree();
+   }
+
+   [ContextMenu("UpdateTree")]
+   private void UpdateTree()
+   {
+      if (_cachedPos == _cachedTransform.position &&
+          _cachedRot == _cachedTransform.rotation.eulerAngles &&
+          _cachedScale == _cachedTransform.localScale)
+      {
+         return;
+      }
       SetAABB();
       Tree.Remove(_index);
       Tree.InsertLeaf(_index,_aabb);
+      _cachedPos = _cachedTransform.position;
+      _cachedRot = _cachedTransform.rotation.eulerAngles;
+      _cachedScale = _cachedTransform.localScale;
+      
+      Debug.Log("yo");
    }
 
    private void SetAABB()
